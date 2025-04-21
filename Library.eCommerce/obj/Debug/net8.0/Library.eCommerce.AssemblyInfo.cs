478af7f85aa3ed4e7e4b701853e550f1c323e041
AbigailUhl <abbyuhl2022@gmail.com>
2025-04-21 19:06:32 -0400

    final submission

--- Library.eCommerce/obj/Debug/net8.0/Library.eCommerce.AssemblyInfo.cs
+++ Library.eCommerce/obj/Debug/net8.0/Library.eCommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f581abe67bbc14ef52dc7f8fa039180a905b733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55940417ee7549590915db005f2084298e8789a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Maui.eCommerceV3/obj/Debug/net8.0-ios/iossimulator-arm64/Maui.eCommerceV3.AssemblyInfo.cs
+++ Maui.eCommerceV3/obj/Debug/net8.0-ios/iossimulator-arm64/Maui.eCommerceV3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maui.eCommerceV3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7f581abe67bbc14ef52dc7f8fa039180a905b733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+55940417ee7549590915db005f2084298e8789a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maui.eCommerceV3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maui.eCommerceV3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
